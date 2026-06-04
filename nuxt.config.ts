5ffe15acbdc751f21ffc94437d91d69a4375f016
murad-sinatra <MuradMorshedd@gmail.com>
2026-06-04 19:12:11 -0400

    update hit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
+import process from 'process'
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
     head: {
       title: 'Crown Valet | Premium Parking On Demand',
       meta: [
